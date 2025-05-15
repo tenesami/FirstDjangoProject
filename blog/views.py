3407534aed53f3819efb0ddb45a9cfd8abd3b238
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2025-05-14 20:40:49 -0600

    add dummy content

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
 
 
+posts = [
+    {
+    'author': 'John Doe',
+    'title': 'Blog Post 1',
+    'content': 'This is the content of the first blog post.',
+    'date_posted': 'August 27, 2023'
+    },
+{
+    'author': 'Jane Smith',
+    'title': 'Blog Post 2',
+    'content': 'This is the content of the second blog post.',
+    'date_posted': 'August 28, 2023'
+},
+{
+    'author': 'Alice Johnson',
+    'title': 'Blog Post 3',
+    'content': 'This is the content of the third blog post.',
+    'date_posted': 'August 29, 2023'
+}]
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
 
 
 
