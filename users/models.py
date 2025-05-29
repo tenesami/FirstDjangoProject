966840635fa4bb228442e7856543c1d857153d37
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2025-05-29 00:14:03 -0600

    add image to home page

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import FileExtensionValidator
-
+from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):
@@ -18,3 +18,17 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
         # return self.user.username
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+#         # This method overrides the save method to resize the image if it exceeds 300x300 pixels.
+#         # The image is resized to fit within a 300x300 pixel box while maintaining its aspect ratio.
+#         # The resized image is then saved back to the same path.
+#         # This ensures that profile pictures are optimized for web use, reducing file size and loading times.
+#         # The image is resized to fit within a 300x300 pixel box while maintaining its aspect ratio.
+#         # This ensures that profile pictures are optimized for web use, reducing file size and loading times.
